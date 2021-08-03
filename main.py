da8615619fc6f3b3ad2a387a3b2d5823c5d509ff
vanajamsp <me_vanaj@yahoo.co.in>
2021-08-03 18:19:52 -0400

    Square function.

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 
-#print("Hello World")
-
-
-##Adding a different comment to see the merge conflict
-
-
-##Test comment
-#Pushing this code from Pycharm.
-
-
-
+print("Hello World")
 name = input("enter your name: ")
 age = input("enter your age: ")
 print("Hi "+ name + "you are " + age)
 
+num1 = int(input("Enter a number"))
+def square(num):
+ return num * num
 
-#check if the version control is enabled.
-##test
+print(square(num1))
 
-TEsted
+
+
+
